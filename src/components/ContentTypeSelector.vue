e35e071af8da7d8c48ae7c9ca014aa3053f2d8e4
maticaba99 <maticaba99@hotmail.com>
2021-04-23 16:55:51 -0300

    asd

--- src/components/ContentTypeSelector.vue
+++ src/components/ContentTypeSelector.vue
@@ -158,5 +158,4 @@ export default {
 </script>
 
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.16.1/axios.min.js"></script>
 <style scoped></style>
