ecb64100da3c683d2e5225bbea00c7f7171f65fd
maticaba99 <maticaba99@hotmail.com>
2021-04-23 14:29:49 -0300

    comiteand

--- src/components/ContentTypeSelector.vue
+++ src/components/ContentTypeSelector.vue
@@ -10,10 +10,13 @@
       :option-height="104"
       :multiple="true"
       :loading="isLoading"
+      :internal-search="false"
+      :close-on-select="false"
       label="name"
       track-by="name"
       :disabled="element.disabled"
       @input="onSelect"
+      @search-change="fetchTypes"
     >
       <template slot="singleLabel">
         <img class="option__image" :src="option.image" :alt="option.name" />
@@ -67,14 +70,40 @@ export default {
     limitText(count) {
       return `and ${count} other countries`;
     },
-    fetchTypes() {
+    fetchTypes(searchTerm) {
       fetch(
         `https://6d1a49bf49e44b74a37bcaa0edf1d9e7.eastus2.azure.elastic-cloud.com:9243/products/_search`,
         {
           headers: {
             "Content-Type": "application/json",
             Authorization: `Basic ZWxhc3RpYzpXNFRDNTVFTUdRUmx5a0F2ZVZaOVVnTjM`
-          }
+          },
+          data: JSON.stringify({
+            from: 0,
+            size: 100,
+            query: {
+              bool: {
+                must: [
+                  {
+                    match: {
+                      "productfields.salsifydata_inheritance_hierarchy_level_id":
+                        "Base"
+                    }
+                  },
+                  {
+                    wildcard: {
+                      "productfields.product_name.en-us": searchTerm
+                    }
+                  },
+                  {
+                    exists: {
+                      field: "productcard"
+                    }
+                  }
+                ]
+              }
+            }
+          })
         }
       )
         .then(response => response.json())
