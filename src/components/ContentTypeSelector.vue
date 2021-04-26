81be799daa16201702217ccc0d55e515fcba6b7f
maticaba99 <maticaba99@hotmail.com>
2021-04-26 12:20:39 -0300

    assada

--- src/components/ContentTypeSelector.vue
+++ src/components/ContentTypeSelector.vue
@@ -154,7 +154,7 @@ export default {
       this.$emit("update:value", value);
     },
     customLabel({ name, id }) {
-      return `${name} – ${id}`;
+      return `${name} -- ${id}`;
     }
   }
 };
