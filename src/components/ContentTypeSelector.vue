e8c5a446df2702dbe636630d00dbbfea6994f937
maticaba99 <maticaba99@hotmail.com>
2021-04-26 12:46:47 -0300

    asddd

--- src/components/ContentTypeSelector.vue
+++ src/components/ContentTypeSelector.vue
@@ -29,7 +29,6 @@
           :src="props.option.image"
           :alt="props.option.name"
         />
-        />
         <span class="option__desc">
           <span class="option__title">{{ props.option.name }}tast</span></span
         ></template
